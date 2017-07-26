4644d3a740d0c428be8cf758685c252e18c82d37
yvan karmouta <yvan.karmouta@gmail.com>
2017-07-26 15:55:13 +0200

    Readme

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_MAIL_SENDER = 'Flask template <flask@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     @staticmethod
